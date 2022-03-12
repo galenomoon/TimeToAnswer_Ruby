f5610a8975b42eb691c2ba6b6ed577e3c941a0c7
galeno <gui.galenocover@gmail.com>
2022-03-12 14:42:37 -0300

    Active Storage - Uploading Photo

--- app/controllers/users_backoffice/profile_controller.rb
+++ app/controllers/users_backoffice/profile_controller.rb
@@ -9,6 +9,14 @@ class UsersBackoffice::ProfileController < UsersBackofficeController
     def update
         if @user.update(params_user)
             bypass_sign_in(@user)
+
+            if params_user[:user_profile_attributes][:avatar]
+            
+                redirect_to users_backoffice_welcome_index_path, notice: "Avatar Atualizado Com Sucesso"
+            else
+                redirect_to users_backoffice_profile_path, notice: "Usuário Atualizado Com Sucesso"
+            
+            end
           redirect_to users_backoffice_profile_path, notice: "Usuário Atualizado Com Sucesso"
           else
           render :edit
@@ -23,7 +31,7 @@ class UsersBackoffice::ProfileController < UsersBackofficeController
 
     def params_user
         params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation,
-        user_profile_attributes: [:id, :address, :gender, :birthdate])
+        user_profile_attributes: [:id, :address, :gender, :birthdate, :avatar])
     end
 
     def verify_password
